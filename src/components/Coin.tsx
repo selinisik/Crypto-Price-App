e5436fe030194f6d90829e3ba85b8220f6a4d98f
selinisik <selin2001a@gmail.com>
2023-03-08 00:03:24 +0300

    Changed the name section of coin card

--- src/components/Coin.tsx
+++ src/components/Coin.tsx
@@ -5,7 +5,7 @@ export const Coin: FunctionComponent<Props> = (Props) => {
   const { name, icon, price, symbol } = Props;
   return (
     <div className="hover:bg-slate-50 hover:text-black px-1 box-shadow-coin flex flex-col justify-between pb-4 w-full sm:w-[290px] md:w-[350px] lg:w-[300px] xl:w-[400px] h-[300px] bg-black text-white m-5 text-center rounded-xl ">
-      <h1 className="text-3xl  pb-3 sm:pb-4 pt-3 ">Name: {name}</h1>
+      <h1 className="text-3xl  pb-3 sm:pb-4 pt-3 ">{name}</h1>
       <img
         src={icon}
         alt={name}
